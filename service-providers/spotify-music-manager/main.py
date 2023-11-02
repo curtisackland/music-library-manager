4fef2cf2d01d3457629064361eb4b9aca366f614
Brandon Nathan Marks <bmarks7@uwo.ca>
2023-11-02 16:31:45 -0400

    added flask routes and dockerfiles

--- service-providers/spotify-music-manager/main.py
+++ service-providers/spotify-music-manager/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/spotifymusicmanager')
+def hello_world():
+    return 'This is the spotify music manager'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
--- service-registry/main.py
+++ service-registry/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/serviceregistry')
+def hello_world():
+    return 'This is the service-registry'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
