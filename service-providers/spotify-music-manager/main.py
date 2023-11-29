80b997f5d83c30e0ba18c0f94dd0cd8f6ed5bfb2
Curtman <curtisackland@rogers.com>
2023-11-29 16:01:12 -0500

    Added endpoints and keep track of active providers (#6)
    * Added endpoints and keep track of active providers
    
    * Added env example files and added heartbeats to apple
    
    * updated docker files
    
    * removed env file from registry

--- service-providers/spotify-music-manager/main.py
+++ service-providers/spotify-music-manager/main.py
@@ -1,23 +1,48 @@
+import os
+import sys
+import threading
+import requests
+import time
 import flask
 from flask_cors import CORS
 import SpotifyAPI
+
 app = flask.Flask(__name__)
 CORS(app)
-import sys
 
 api = SpotifyAPI.SpotifyAPI()
+
+TIME_BETWEEN_REQUESTS = 30
+
+SEND_HEARTBEAT = True
+
+
+def sendHeartbeat() -> None:
+    global SEND_HEARTBEAT
+    while SEND_HEARTBEAT:
+        data = {
+            "url": os.environ.get('URL'),
+            "type": "register",
+            "name": "Spotify Library Manager"
+        }
+        print(requests.post(os.environ.get('REGISTRY_URL'), json=data))
+        time.sleep(TIME_BETWEEN_REQUESTS)
+
 
 @app.route('/playlists')
 def playlists():
     return api.getPlaylists(flask.request.args.get("username"))
 
+
 @app.route('/playlist')
 def playlist():
     return api.getPlaylist(flask.request.args.get("playlist_id"))
 
+
 @app.route('/client_id')
 def client_id():
     return api.getClientID()
+
 
 @app.route('/get_user_token')
 def get_user_token():
@@ -30,6 +55,16 @@ def get_user_token():
 
 
 if __name__ == '__main__':
+    t1 = threading.Thread(target=sendHeartbeat)
+    t1.start()
     app.run(host='0.0.0.0', port=3001)
-    #TODO: Add deregistration heartbeat here
 
+    # unregisters the service from the registry on stop
+    SEND_HEARTBEAT = False
+    endRequest = {
+        "url": os.environ.get('URL'),
+        "type": "unregister",
+        "name": "Spotify Library Manager"
+    }
+    requests.post(os.environ.get('REGISTRY_URL'), json=endRequest)
+    t1.join()
