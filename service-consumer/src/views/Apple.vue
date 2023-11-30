80a157486426d06da1614cb9347dbee77b9ea30d
evangoldrick <84489887+evangoldrick@users.noreply.github.com>
2023-11-30 01:20:09 -0500

    Added apple user auth with example (#8)
    * Added apple user auth with example
    
    * Removed testing code

--- service-consumer/src/views/Apple.vue
+++ service-consumer/src/views/Apple.vue
@@ -1,11 +1,43 @@
-<script setup>
-
-</script>
-
 <template>
+  <h1>Apple Music takes 2 HOURS to update</h1>
   {{ $store.getters.getBackendURL }}
+  {{ eq }}
 </template>
 
-<style scoped>
+<script>
+import axios from "axios";
 
-</style>
+
+export default {
+  name: "Apple",
+  methods: {
+    async getMusicUserToken() {
+      return await MusicKit.getInstance().authorize();
+    },
+    async exampleQuery() {
+      let x = await axios.get("http://localhost:3002/getPlaylists", {params:{"user_token":await this.getMusicUserToken()}});
+      console.log(x);
+      console.log(x.data);
+      return x;
+    }
+  },
+  data() {
+    return {
+      eq:null,
+    }
+  },
+  async mounted() {
+    while (MusicKit == undefined); // Wait for apple music to load
+    await MusicKit.configure({
+      developerToken: (await axios.get("http://localhost:3002/getDeveloperToken")).data,
+      app: {
+        name: 'Apple Muisc',
+        build: '0.0.1',
+      },
+    });
+
+    this.eq = await this.exampleQuery()
+  }
+}
+</script>
+
---
+++ service-providers/apple-music-manager/AppleAPI.py
@@ -0,0 +1,64 @@
+import requests
+import jwt
+import json
+import datetime
+
+CONFIG_FILE = "secrets/config.json"
+
+def getConfig():
+    with open(CONFIG_FILE, "r") as inConfigFile:
+        return json.load(inConfigFile)
+
+def getPrivateKey() -> str:
+        with open(getConfig()["privateKeyFile"], "r") as inKeyFile:
+            return inKeyFile.read()
+
+def generateNewDeveloperBearer():
+    headers = {
+        "kid":getConfig()["kid"]
+    }
+    iat = datetime.datetime.now().timestamp()
+    body = {
+        "iss": getConfig()["iss"],
+        "iat": iat,
+        "exp": iat + 2592000, # 1 month
+    }
+
+    privateKey = getPrivateKey()
+    bearer = jwt.encode(body, headers=headers, key=privateKey, algorithm="ES256")
+
+    bearerJson = {
+        "bearer":bearer,
+        "exp":iat + 2592000,
+    }
+
+    with open(getConfig()["bearerCache"], "w") as outBearerFile:
+        json.dump(bearerJson, outBearerFile)
+
+    return bearerJson
+
+def getBearerToken() -> str:
+    try:
+        with open(getConfig()["bearerCache"], "r") as inBearerFile:
+            j = json.load(inBearerFile)
+            return j["bearer"]
+    except FileNotFoundError:
+        return generateNewDeveloperBearer()["bearer"]
+
+def createDevAuth(devBearer):
+    return {"Authorization":"Bearer " + devBearer}
+
+def createMutAuth(devBearer, mutToken):
+    return {"Authorization":"Bearer " + devBearer, "Music-User-Token": mutToken}
+
+
+class AppleAPI:
+    def __init__(self):
+        self._devBearer = getBearerToken()
+
+    def getPlaylists(self, mutToken):
+        result = requests.get("https://api.music.apple.com/v1/me/library/playlists", headers=createMutAuth(self._devBearer, mutToken))
+        print(result.text, flush=True)
+        data = result.json()
+        return data
+
